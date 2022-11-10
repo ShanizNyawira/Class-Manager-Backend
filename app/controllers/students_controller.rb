103b924935efc845d6f36d263d639891ba6d8bd4
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-11-10 18:52:52 +0300

    add routes for students model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,34 +1,34 @@
-# class StudentsController < Sinatra::Base
-#   set :default_content_type, 'application/json'
+class StudentsController < Sinatra::Base
+  set :default_content_type, 'application/json'
   
-#   # Add your routes here
+  # Add your routes here
 
-#   get "/students" do
-#     students = Student.all
-#     students.to_json
-#   end
+  get "/students" do
+    students = Student.all
+    students.to_json
+  end
 
-#   get "/students/:id" do
-#     student = Student.find(params[:id])
-#     student.to_json
-#   end
+  get "/students/:id" do
+    student = Student.find(params[:id])
+    student.to_json
+  end
 
-#   post "/students" do
-#     student = Student.create(name: params[:name], email: params[:email], age: params[:age])
-#     student.to_json
-#   end
+  post "/students" do
+    student = Student.create(name: params[:name], email: params[:email], age: params[:age])
+    student.to_json
+  end
 
-#   patch "/students/:id" do
-#     student = Student.find(params[:id])
-#     student.update(name: params[:name], email: params[:email], age: params[:age])
-#     student.to_json
-#   end
+  patch "/students/:id" do
+    student = Student.find(params[:id])
+    student.update(name: params[:name], email: params[:email], age: params[:age])
+    student.to_json
+  end
 
-#   delete "/students/:id" do
-#     student = Student.find(params[:id])
-#     student.destroy
-#     student.to_json
-#   end
+  delete "/students/:id" do
+    student = Student.find(params[:id])
+    student.destroy
+    student.to_json
+  end
 
 
-# end
+end
