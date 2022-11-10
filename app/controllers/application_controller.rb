adec160b08ef44c5d6229db43a6b80b7727066a5
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-11-10 18:41:35 +0300

    create file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+# class ApplicationController < Sinatra::Base
+#   set :default_content_type, 'application/json'
+  
+#   # Add your routes here
+#   get "/" do
+#     "Welcome to our class manager app!"
+#   end
+
+
+# end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+puts "🌱 Seeding spices..."
+
+# course1 = Course.create(course_name: "Italian", course_code: 123, course_description: "Learn Italian", teacher_id: 1, student_id: 1)
+# course2 = Course.create(course_name: "Spanish", course_code: 456, course_description: "Learn Spanish", teacher_id: 2, student_id: 2)
+# course3 = Course.create(course_name: "French", course_code: 789, course_description: "Learn French", teacher_id: 3, student_id: 3)
+
+# teacher1 = Teacher.create(name: "Mr. Smith", email: "smithy@gmail.com", phone_number: 8234567890)
+# teacher2 = Teacher.create(name: "Mrs. Jones", email: "jones@gmail.com", phone_number: 9234567890)
+# teacher3 = Teacher.create(name: "Mr. Brown", email: "brown@gmail.com", phone_number: 6234567890)
+
+# student1 = Student.create(name: "John", email: "john@gmail.com", age: 18, course_id: 1)
+# student2 = Student.create(name: "Jane", email: "jane@gmail.com", age: 19, course_id: 2)
+# student3 = Student.create(name: "Jack", email: "jack@gmail.com", age: 20, course_id: 3)
+# student4 = Student.create(name: "Jill", email: "jill@gmail.com", age: 21, course_id: 1)
+
+puts "✅ Done seeding!"
