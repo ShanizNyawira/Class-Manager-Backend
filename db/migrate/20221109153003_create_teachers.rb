f497620534f41dca6d13eed44e5ea55e8327a920
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-11-10 18:45:05 +0300

    add tables to the db

--- db/migrate/20221109153003_create_teachers.rb
+++ db/migrate/20221109153003_create_teachers.rb
@@ -1,12 +1,12 @@
-# class CreateTeachers < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :teachers do |t|
-#       t.string :name
-#       t.string :email
-#       t.integer :phone_number
+class CreateTeachers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone_number
             
-#       t.timestamps
-#     end
-#   end
+      t.timestamps
+    end
+  end
 
-# end
+end
--- db/migrate/20221109153138_create_students.rb
+++ db/migrate/20221109153138_create_students.rb
@@ -1,12 +1,12 @@
-# class CreateStudents < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :students do |t|
-#       t.string :name
-#       t.string :email
-#       t.integer :age
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.integer :age
 
-#       t.timestamps
-#     end
-#   end
+      t.timestamps
+    end
+  end
 
-# end
+end
