8e753da922bb78151f7c2ecef3119a055cd7503b
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-11-10 18:52:00 +0300

    modify app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
-# class ApplicationController < Sinatra::Base
-#   set :default_content_type, 'application/json'
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
   
-#   # Add your routes here
-#   get "/" do
-#     "Welcome to our class manager app!"
-#   end
+  # Add your routes here
+  get "/" do
+    "Welcome to our class manager app!"
+  end
 
 
-# end
+end
